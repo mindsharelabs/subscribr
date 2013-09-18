689ff4e4d2fffe4bc70c1c020299f2ff81796356
Damian <info@mindsharestudios.com>
2013-09-18 12:34:01 -0600

    options page updates

--- views/options-page.php
+++ views/options-page.php
@@ -1,19 +1,19 @@
 <?php
 /**
  * options-page.php
- * 
- * @created 9/17/13 4:11 PM
- * @author Mindshare Studios, Inc.
+ *
+ * @created   9/17/13 4:11 PM
+ * @author    Mindshare Studios, Inc.
  * @copyright Copyright (c) 2013
- * @link http://www.mindsharelabs.com/documentation/
- * 
+ * @link      http://www.mindsharelabs.com/documentation/
+ *
  */
 
 $es_options = new mindshare_options_framework(
 	array(
 		 'project_name' => ES_PLUGIN_NAME,
 		 'menu'         => 'settings',
-		 'page_title'   => ES_PLUGIN_NAME.' '.__('Settings', 'email-subscribe'),
+		 'page_title'   => sprintf(__('%s Settings', 'email-subscribe'), ES_PLUGIN_NAME),
 		 'menu_title'   => ES_PLUGIN_NAME,
 		 'capability'   => 'manage_options',
 		 'option_group' => ES_OPTIONS,
@@ -32,7 +32,7 @@ $es_options->TabsListing(
 		 'links' =>
 		 array(
 			 sanitize_title($es_options_label)  => __($es_options_label, 'email-subscribe'),
-			 sanitize_title($es_options_label2)  => __($es_options_label2, 'email-subscribe'),
+			 sanitize_title($es_options_label2) => __($es_options_label2, 'email-subscribe'),
 		 )
 	)
 );
@@ -47,17 +47,16 @@ $es_options->Title($es_options_label);
 $es_options->addText(
 	'from_name',
 	array(
-		 'name' => 'From Name',
+		 'name' => __('From Name', 'email-subscribe'),
 		 'std'  => get_bloginfo('name'),
 		 'desc' => ''
 	)
 );
 
-
 $es_options->addText(
 	'from_email',
 	array(
-		 'name' => 'From Email',
+		 'name' => __('From Email', 'email-subscribe'),
 		 'std'  => get_option('admin_email'),
 		 'desc' => ''
 	)
@@ -71,8 +70,7 @@ $es_options->CloseTab();
 $es_options->OpenTab(sanitize_title($es_options_label2));
 $es_options->Title($es_options_label2);
 
-
-// taxonomy choices @todo add select all/none toggle to Mindshare Options Framewwork
+// taxonomy choices @todo add select all/none toggle to Mindshare Options Framework
 $taxonomies = get_taxonomies();
 $disabled_taxonomies = array('nav_menu', 'post_format', 'link_category');
 $taxonomies = array_diff($taxonomies, $disabled_taxonomies);
@@ -80,11 +78,11 @@ $es_options->addTaxonomy(
 	'taxonomies',
 	array(
 		 'taxonomy' => $taxonomies,
-		 'type'  => 'checkbox_list',
+		 'type'     => 'checkbox_list',
 	),
 	array(
-		 'name' => 'Enabled Terms',
-		 'desc' => 'Choose the terms you wish to allow users to subscribe on their profile.'
+		 'name' => __('Enabled Terms', 'email-subscribe'),
+		 'desc' => __('Choose the terms you want to allow users to subscribe from their profiles.', 'email-subscribe')
 	),
 	FALSE
 );
@@ -97,28 +95,26 @@ $es_options->CloseTab();
 $es_options->HelpTab(
 	array(
 		 'id'      => 'es-help-tab',
-		 'title'   => ES_PLUGIN_NAME.' Documentation',
-		 'content' => '<p>'.ES_PLUGIN_NAME.' documentation is available online at <a href="http://mindsharelabs.com/topics/'.ES_PLUGIN_SLUG.'/" target="_blank">http://mindsharelabs.com/topics/'.ES_PLUGIN_SLUG.'/</a></p>'
+		 'title'   => sprintf(__('%s Documentation', 'email-subscribe'), ES_PLUGIN_NAME),
+		 'content' => sprintf(__('<p>%1$s documentation is available online at <a href="http://mindsharelabs.com/topics/%2$s/" target="_blank">http://mindsharelabs.com/topics/%2$s/</a></p>', 'email-subscribe'), ES_PLUGIN_NAME, ES_PLUGIN_SLUG)
 	)
 );
 $es_options->HelpTab(
 	array(
 		 'id'      => 'es-support-tab',
-		 'title'   => 'Support Forum',
-		 'content' => '<p>Get support on the WordPress.org forums: <a href="http://wordpress.org/support/plugin/'.ES_PLUGIN_SLUG.'" target="_blank">http://wordpress.org/support/plugin/'.ES_PLUGIN_SLUG.'</a></p><p>To get premium one-on-one support, contact us: <a href="http://mind.sh/are/contact/" target="_blank">http://mind.sh/are/contact/</a></p>'
+		 'title'   => __('Support Forum', 'email-subscribe'),
+		 'content' => sprintf(__('<p>Get support on the WordPress.org forums: <a href="http://wordpress.org/support/plugin/%1$s" target="_blank">http://wordpress.org/support/plugin/%1$s</a></p><p>To get premium one-on-one support, contact us: <a href="http://mind.sh/are/contact/" target="_blank">http://mind.sh/are/contact/</a></p>', 'email-subscribe'), ES_PLUGIN_SLUG)
 	)
 );
-$secure_tab_content = "<p>Get the Mindshare Team to secure and protect your WordPress site for $9.95/month: <a href='http://mind.sh/are/wordpress-security-and-backup-service/check/?url=".get_bloginfo("url")."&amp;active=0&amp;sale=1&amp;d=".str_replace(
-		array(
-			 "http://",
-			 "https://"
-		),
-		"",
-		get_home_url())."' target='_blank'>http://mind.sh/are/wordpress-security-and-backup-service/</a></p>";
+$secure_tab_content = sprintf(__('<p>Get the Mindshare Team to secure and protect your WordPress site for $9.95/month: <a href="http://mind.sh/are/wordpress-security-and-backup-service/check/?url=%1$s&amp;active=0&amp;sale=1&amp;d=%2$s" target="_blank">http://mind.sh/are/wordpress-security-and-backup-service/</a></p>', 'email-subscribe'), get_bloginfo("url"), str_replace(array(
+																																																																																														   "http://",
+																																																																																														   "https://"
+																																																																																													  ), "", get_home_url()));
+
 $es_options->HelpTab(
 	array(
 		 'id'      => 'es-security-tab',
-		 'title'   => 'Protect Your Site',
+		 'title'   => __('Protect Your Site', 'email-subscribe'),
 		 'content' => $secure_tab_content
 	)
 );
